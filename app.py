b201be2aeb3c0dbdde2f8c63ff8ef589168bbb1e
Ayshaabdulazeez <110766529+Ayshaabdulazeez@users.noreply.github.com>
2023-11-27 10:33:05 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,request,render_template
+import pickle
+import numpy as np
+
+app=Flask(__name__)
+model=pickle.load(open('linear_regression_model.pkl', 'rb'))
+
+@app.route('/')#http://www.google.com/
+def home():
+    return render_template('index.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final_features=[np.array(int_features)]
+    prediction=model.predict(final_features)
+
+    output=round(prediction[0],2)
+    return render_template('index.html',prediction_text='Price charged should be ${}'.format(output))
+
+if __name__=="__main__":
+    app.run(port=100,debug=True)
+app.run(port=100)
